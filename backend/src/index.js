b499d56ed6528cc2eb898446166628ecdf53b237
Joao-gabriel-15 <caetanojoaogabriel@gmail.com>
2020-03-28 19:10:53 -0300

    Back-end da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const routes = require('./routes'); //importando o arquivo routes.js
 const app = express(); //instanciando a var app e chamando a var express
 
 app.use(cors());
+
 app.use(express.json()); //Antes de todas as requisições, o express converter o json em objeto JavaScript
 app.use(routes);
 
